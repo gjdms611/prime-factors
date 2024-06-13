39d7d3e6ec9ea9126b666f14e26e88c24fa01b03
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 09:56:46 +0900

    [feature] add 12

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class PrimeFactor:
         factors = []
         if number > 1:
             diviser = 2
-            if number == 4 or number == 6 or number == 9:
+            if number == 4 or number == 6 or number == 9 or number == 12:
                 while number > 1:
                     while not number % diviser:
                         factors.append(diviser)
