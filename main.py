1f8e4c6a4644c3aceb9a48db5765a0fa118fa7e1
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 09:55:57 +0900

    [refactoring] generalize code

--- main.py
+++ main.py
@@ -3,19 +3,12 @@ class PrimeFactor:
         factors = []
         if number > 1:
             diviser = 2
-            if number == 4:
-                while not number % diviser:
-                    factors.append(diviser)
-                    number //= diviser
-            elif number == 6:
+            if number == 4 or number == 6 or number == 9:
                 while number > 1:
-                    if not number % diviser:
+                    while not number % diviser:
                         factors.append(diviser)
                         number //= diviser
                     diviser += 1
-            elif number == 9:
-                factors.append(3)
-                factors.append(3)
             else:
                 factors.append(number)
         return factors
