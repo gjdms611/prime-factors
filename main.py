b41f22cd3b5efa40e52dd2b91b7aa3cd9c1cc6ad
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 09:49:41 +0900

    [feature] add 6

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ class PrimeFactor:
             while not number % 2:
                 factors.append(2)
                 number //= 2
+        elif number == 6:
+            factors.append(2)
+            factors.append(3)
         elif number > 1:
             factors.append(number)
         return factors
