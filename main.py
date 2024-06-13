4186b7c30fce443116e29533a2378cffeeb85c79
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 09:57:28 +0900

    [refactoring] generalize code

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 class PrimeFactor:
     def of(self, number):
         factors = []
-        if number > 1:
-            diviser = 2
-            if number == 4 or number == 6 or number == 9 or number == 12:
-                while number > 1:
-                    while not number % diviser:
-                        factors.append(diviser)
-                        number //= diviser
-                    diviser += 1
-            else:
-                factors.append(number)
+        diviser = 2
+        while number > 1:
+            while not number % diviser:
+                factors.append(diviser)
+                number //= diviser
+            diviser += 1
         return factors
