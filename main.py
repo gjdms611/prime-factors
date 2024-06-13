9229a3064eb4149e402670d47ed7ce1748bf9494
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 09:39:36 +0900

    [feature] add 2

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 class PrimeFactor:
-    def of(self, num):
-        return []
+    def of(self, number):
+        factors = []
+        if number == 2:
+            factors.append(2)
+        return factors
--- test_main.py
+++ test_main.py
@@ -4,6 +4,10 @@ from main import PrimeFactor
 
 
 class TestPrimeFactor(TestCase):
-    def test_prime_factor(self):
+    def test_prime_factor_of_1(self):
         prime_factor = PrimeFactor()
         self.assertEqual(prime_factor.of(1), [])
+
+    def test_prime_factor_of_2(self):
+        prime_factor = PrimeFactor()
+        self.assertEqual(prime_factor.of(2), [2])
