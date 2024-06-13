c6dbf2b52dc1e4b39ba918761111769761ff1e4c
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 09:58:05 +0900

    [refactoring] fix typo

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 class PrimeFactor:
     def of(self, number):
         factors = []
-        diviser = 2
+        divisor = 2
         while number > 1:
-            while not number % diviser:
-                factors.append(diviser)
-                number //= diviser
-            diviser += 1
+            while not number % divisor:
+                factors.append(divisor)
+                number //= divisor
+            divisor += 1
         return factors
